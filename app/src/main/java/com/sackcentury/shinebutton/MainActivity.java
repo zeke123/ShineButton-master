42420aa74eeacfae2a1e292faa8e0239cb5c6d72
zhoujian <1064791247@qq.com>
2016-08-15 14:50:53 +0800

    zeke  第二次提交

---
+++ app/src/main/java/com/sackcentury/shinebutton/MainActivity.java
@@ -0,0 +1,118 @@
+package com.sackcentury.shinebutton;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.sackcentury.shinebuttonlib.ShineButton;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = "MainActivity";
+    ShineButton shineButton;
+    ShineButton porterShapeImageView1;
+    ShineButton porterShapeImageView2;
+    ShineButton porterShapeImageView3;
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        shineButton = (ShineButton) findViewById(R.id.po_image0);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.wrapper);
+
+        if (shineButton != null)
+            shineButton.init(this);
+        porterShapeImageView1 = (ShineButton) findViewById(R.id.po_image1);
+        if (porterShapeImageView1 != null)
+            porterShapeImageView1.init(this);
+        porterShapeImageView2 = (ShineButton) findViewById(R.id.po_image2);
+        if (porterShapeImageView2 != null)
+            porterShapeImageView2.init(this);
+        porterShapeImageView3 = (ShineButton) findViewById(R.id.po_image3);
+        if (porterShapeImageView3 != null)
+            porterShapeImageView3.init(this);
+
+        listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(new ListAdapter());
+
+        ShineButton shineButtonJava = new ShineButton(this);
+
+        shineButtonJava.setBtnColor(Color.GRAY);
+        shineButtonJava.setBtnFillColor(Color.RED);
+        shineButtonJava.setShapeResource(R.raw.heart);
+        shineButtonJava.setAllowRandomColor(true);
+        shineButtonJava.setShineSize(100);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100, 100);
+        shineButtonJava.setLayoutParams(layoutParams);
+        if (linearLayout != null) {
+            linearLayout.addView(shineButtonJava);
+        }
+
+
+
+        shineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "click");
+            }
+        });
+        shineButton.setOnCheckStateChangeListener(new ShineButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(View view, boolean checked) {
+                Log.e(TAG, "click "+checked);
+            }
+        });
+
+        porterShapeImageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "click");
+            }
+        });
+        porterShapeImageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "click");
+            }
+        });
+
+
+    }
+
+    class ListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 20;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item, null);
+            }
+
+            return view;
+        }
+    }
+}
